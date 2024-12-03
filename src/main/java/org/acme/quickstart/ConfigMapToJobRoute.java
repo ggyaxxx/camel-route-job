ae830fdb1b52873f671721e4f40b58fec53b7e33
asdf <asdf@asdf.com>
2024-12-03 17:04:33 +0100

    .

--- src/main/java/org/acme/quickstart/ConfigMapToJobRoute.java
+++ src/main/java/org/acme/quickstart/ConfigMapToJobRoute.java
@@ -3,27 +3,40 @@ package org.acme.quickstart;
 import org.apache.camel.builder.RouteBuilder;
 import org.apache.camel.component.kubernetes.KubernetesConstants;
 
+import java.util.Map;
+
 public class ConfigMapToJobRoute extends RouteBuilder {
 
     @Override
     public void configure() throws Exception {
 
-        from("timer:configMapTimer?period=10000") // Esegue ogni 10 secondi
+        from("timer:configMapTimer?period=10000")
                 .log("Fetching ConfigMap...")
 
                 // Legge la ConfigMap dal namespace
-                .setHeader(KubernetesConstants.KUBERNETES_NAMESPACE_NAME, constant("camel-route-job"))
-                .setHeader(KubernetesConstants.KUBERNETES_CONFIGMAP_NAME, constant("job-config"))
+                .setHeader("CamelKubernetesNamespaceName", constant("camel-route-job"))
+                .setHeader("CamelKubernetesConfigMapName", constant("job-config"))
                 .to("kubernetes-config-maps://kubernetes.default.svc")
                 .log("ConfigMap fetched: ${body}")
 
-                // Estrai la definizione del Job dalla ConfigMap
-                .setBody(simple("${body.data['job-definition']}"))
+                // Processa il corpo senza usare `simple`
+                .process(exchange -> {
+                    // Ottieni il corpo della risposta come mappa
+                    Map<String, Object> body = exchange.getMessage().getBody(Map.class);
+                    if (body != null && body.containsKey("data")) {
+                        Map<String, String> data = (Map<String, String>) body.get("data");
+                        String jobDefinition = data.get("job-definition");
+                        exchange.getMessage().setBody(jobDefinition);
+                    } else {
+                        throw new RuntimeException("ConfigMap data not found or invalid format");
+                    }
+                })
                 .log("Job definition extracted: ${body}")
 
-                // Invia la definizione del Job all'endpoint Kubernetes
-                .setHeader(KubernetesConstants.KUBERNETES_NAMESPACE_NAME, constant("camel-rotta"))
+                // Crea il Job su OpenShift
+                .setHeader("CamelKubernetesNamespaceName", constant("camel-route-job"))
                 .to("kubernetes-job://kubernetes.default.svc?operation=createJob")
                 .log("Job created successfully!");
     }
-}
+    }
+
