f557914d4e70344653888fdc4367a82c7bd675db
asdf <asdf@asdf.com>
2024-12-04 13:47:30 +0100

    .

--- src/main/java/org/acme/quickstart/ConfigMapToJobRoute.java
+++ src/main/java/org/acme/quickstart/ConfigMapToJobRoute.java
@@ -22,15 +22,15 @@ public class ConfigMapToJobRoute extends RouteBuilder {
                 .to("kubernetes-config-maps://kubernetes.default.svc?operation=getConfigMap")
                 .log("Contenuto della ConfigMap: ${body}")
                 .process(exchange -> {
+                    // Lascio comunque questo blocchetto di codice per mostrare come recuperare la ConfigMap
+
                     ConfigMap configMap = exchange.getMessage().getBody(ConfigMap.class);
                     if (configMap != null && configMap.getData() != null) {
-                        // Recupera la definizione del job dalla ConfigMap
                         String jobName = "camel-job-" + (int)(Math.random() * 9000 + 1000);
                         exchange.getMessage().setHeader(KubernetesConstants.KUBERNETES_JOB_NAME, jobName);
                         exchange.getMessage().setHeader(KubernetesConstants.KUBERNETES_NAMESPACE_NAME, "camel-rotta");
                         exchange.getMessage().setHeader(KubernetesConstants.KUBERNETES_JOB_LABELS, Map.of("app", "camel-job"));
 
-                        // Genera dinamicamente lo spec del Job
                         JobSpec jobSpec = generateJobSpec();
                         exchange.getMessage().setHeader(KubernetesConstants.KUBERNETES_JOB_SPEC, jobSpec);
                     } else {
@@ -42,6 +42,11 @@ public class ConfigMapToJobRoute extends RouteBuilder {
 
 
     }
+
+    /* LA ROTTA FUNZIONA SOLO CREANDO IL JOB CON L'UTILIZZO DELL'OGGETTO JOBSPEC. Se si vuole utilizare la ConfigMap
+       allora si dovrà per forza parsarla ed estrarre i dati.
+     */
+
 
     private JobSpec generateJobSpec() {
         JobSpec jobSpec = new JobSpec();
