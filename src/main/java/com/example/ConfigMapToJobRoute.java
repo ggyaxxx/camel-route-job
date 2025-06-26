a4208d982ffbc398be26d399f9afe86a24fb6d7d
dscrimie <dscrimie@redhat.com>
2025-06-26 09:37:11 +0200

    .

--- src/main/java/com/example/ConfigMapToJobRoute.java
+++ src/main/java/com/example/ConfigMapToJobRoute.java
@@ -6,6 +6,7 @@ import io.fabric8.kubernetes.client.KubernetesClient;
 import io.fabric8.kubernetes.client.utils.Serialization;
 import org.apache.camel.builder.RouteBuilder;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
@@ -17,6 +18,12 @@ public class ConfigMapToJobRoute extends RouteBuilder {
     public ConfigMapToJobRoute(KubernetesClient kubernetesClient) {
         this.kubernetesClient = kubernetesClient;
     }
+
+    @Value("${myapp.camel.file.input.directory:input}")
+    private String inputDirectory;
+
+    @Value("${myapp.camel.file.processing.directory:processing}")
+    private String processingDirectory;
 
     @Override
     public void configure() throws Exception {
@@ -49,5 +56,22 @@ public class ConfigMapToJobRoute extends RouteBuilder {
                     }
                 })
                 .log("${body}");
+
+        from("file:" + inputDirectory +
+                "?preMove=" + processingDirectory + "/${file:name}" +
+                "&readLock=changed" +
+                "&readLockCheckInterval=1000" +
+                "&readLockMinLength=1" +
+                "&readLockMinAge=2000" +
+                "&delete=false" +
+                "&noop=true" +
+                "&initialDelay=1000&delay=5000")
+
+                .log("File originale ${header.CamelFileNameOriginal} pre-mosso in: " + processingDirectory)
+                .log("Inizio processamento per il file: ${header.CamelFilePath}") // CamelFilePath punta al file in processingDirectory
+
+
+                .log("Avvio del job Kubernetes per il file ${header.CamelFileNameOriginal}");
+
     }
 }
